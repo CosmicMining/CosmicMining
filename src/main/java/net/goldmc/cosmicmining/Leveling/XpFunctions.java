ecfc1466691589ff8dffec18355cc73d3521a79c
Ross <rosslwheelerjr@gmail.com>
2022-03-09 21:35:15 -0700

    Added Config Stuff

--- src/main/java/net/goldmc/cosmicmining/Leveling/XpFunctions.java
+++ src/main/java/net/goldmc/cosmicmining/Leveling/XpFunctions.java
@@ -1,7 +1,12 @@
 package net.goldmc.cosmicmining.Leveling;
 
+import net.goldmc.cosmicmining.Config.Config;
 import net.goldmc.cosmicmining.Database.LoadPlayerData;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static java.lang.Float.parseFloat;
@@ -19,5 +24,20 @@ public class XpFunctions {
         float percentage = ( playerData[1] / amountOfXpToNextLevel);
         System.out.printf(String.valueOf(percentage));
         return percentage;
+    }
+
+    public int giveXpForOre(Player p) {
+        FileConfiguration Config = net.goldmc.cosmicmining.Config.Config.getCustomConfig1();
+
+        Map<String, Integer> hm
+                = new HashMap<String, Integer>();
+        hm.put("COAL", Config.getInt("XpPerBlock.Coal"));
+        hm.put("IRON", Config.getInt("XpPerBlock.Iron"));
+        hm.put("LAPIS", Config.getInt("XpPerBlock.Lapis"));
+        hm.put("REDSTONE", Config.getInt("XpPerBlock.Redstone"));
+        hm.put("GOLD", Config.getInt("XpPerBlock.Gold"));
+        hm.put("DIAMOND", Config.getInt("XpPerBlock.Diamond"));
+        hm.put("EMERALD", Config.getInt("XpPerBlock.Emerald"));
+        return -1;
     }
 }
